fd8a26a4eeb263c1816ede4f1dae513e79d6a196
anatolii.korotkyi <tolik1794@gmail.com>
2026-05-06 14:15:15 +0300

    Add `ProductPerformanceFixtures` for bulk product generation and enhance `ProductFilterType` with advanced search capabilities

--- src/Form/Admin/FilterType/ProductFilterType.php
+++ src/Form/Admin/FilterType/ProductFilterType.php
@@ -2,8 +2,12 @@
 
 namespace App\Form\Admin\FilterType;
 
+use App\Entity\Category;
 use App\Entity\Product;
+use App\Entity\Store;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,59 @@ class ProductFilterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('code')
-            ->add('category')
-            ->add('store')
+            ->add('name', SearchType::class, [
+				'label' => 'Name',
+	            'required' => false,
+	            'mapped' => false,
+	            'query_callback' => function (QueryBuilder $qb, mixed $value) {
+		            if ($value) {
+			            $rootAlias = current($qb->getRootAliases());
+
+			            $qb->andWhere(sprintf('%s.name like :name', $rootAlias))
+				            ->setParameter('name', '%' . $value . '%');
+		            }
+	            },
+            ])
+            ->add('code', SearchType::class, [
+				'label' => 'Code',
+	            'required' => false,
+	            'mapped' => false,
+	            'query_callback' => function (QueryBuilder $qb, mixed $value) {
+		            if ($value) {
+			            $rootAlias = current($qb->getRootAliases());
+
+			            $qb->andWhere(sprintf('%s.code like :code', $rootAlias))
+				            ->setParameter('code', '%' . $value . '%');
+		            }
+	            },
+            ])
+            ->add('category', null, [
+				'label' => 'Category',
+	            'required' => false,
+	            'mapped' => false,
+	            'query_callback' => function (QueryBuilder $qb, mixed $value) {
+		            if ($value instanceof Category) {
+			            $rootAlias = current($qb->getRootAliases());
+
+			            $qb->andWhere(sprintf('%s.category = :category', $rootAlias))
+				            ->setParameter('category', $value);
+		            }
+	            },
+            ])
+            ->add('store', null, [
+				'label' => 'Store',
+	            'required' => false,
+	            'mapped' => false,
+	            'query_callback' => function (QueryBuilder $qb, mixed $value) {
+		            if ($value instanceof Store) {
+			            $rootAlias = current($qb->getRootAliases());
+
+			            $qb->andWhere(sprintf('%s.store = :filterStore', $rootAlias))
+				            ->setParameter('filterStore', $value);
+		            }
+	            },
+            ])
+	        ->setMethod('GET')
         ;
     }
 
@@ -23,6 +76,7 @@ class ProductFilterType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Product::class,
+	        'csrf_protection' => false,
         ]);
     }
 }
